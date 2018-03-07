784d66f5cbf51c7de8c30eb5647618a766f0cbc3
Sam Kinneman <skinneman28@gmail.com>
2018-03-06 16:22:18 -0800

    Add files via upload

---
+++ phasor_measure.c
@@ -0,0 +1,398 @@
+// ADC.c:  Shows how to use the 14-bit ADC.  This program
+// measures the voltage from some pins of the EFM8LB1 using the ADC.
+//
+// (c) 2008-2018, Jesus Calvino-Fraga
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <EFM8LB1.h>
+#include <time.h>
+#include <math.h>
+
+// ~C51~  
+
+#define SYSCLK 72000000L
+#define BAUDRATE 115200L
+
+#define LCD_RS P2_6
+// #define LCD_RW Px_x // Not used in this code.  Connect to GND
+#define LCD_E  P2_5
+#define LCD_D4 P2_4
+#define LCD_D5 P2_3
+#define LCD_D6 P2_2
+#define LCD_D7 P2_1
+#define CHARS_PER_LINE 16
+
+
+char _c51_external_startup (void)
+{
+	// Disable Watchdog with key sequence
+	SFRPAGE = 0x00;
+	WDTCN = 0xDE; //First key
+	WDTCN = 0xAD; //Second key
+  
+	VDM0CN=0x80;       // enable VDD monitor
+	RSTSRC=0x02|0x04;  // Enable reset on missing clock detector and VDD
+
+	#if (SYSCLK == 48000000L)	
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
+		SFRPAGE = 0x00;
+	#elif (SYSCLK == 72000000L)
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
+		SFRPAGE = 0x00;
+	#endif
+	
+	#if (SYSCLK == 12250000L)
+		CLKSEL = 0x10;
+		CLKSEL = 0x10;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 24500000L)
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 48000000L)	
+		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x07;
+		CLKSEL = 0x07;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 72000000L)
+		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x03;
+		CLKSEL = 0x03;
+		while ((CLKSEL & 0x80) == 0);
+	#else
+		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+	#endif
+	
+	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
+	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
+	XBR1     = 0X00;
+	XBR2     = 0x40; // Enable crossbar and weak pull-ups
+
+	// Configure Uart 0
+	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
+		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+	#endif
+	SCON0 = 0x10;
+	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
+	TL1 = TH1;      // Init Timer1
+	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
+	TMOD |=  0x20;                       
+	TR1 = 1; // START Timer1
+	TI = 1;  // Indicate TX0 ready
+  	
+	return 0;
+}
+// Uses Timer3 to delay <us> micro-seconds. 
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
+	CKCON0|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN0 & 0x80));  // Wait for overflow
+		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+	}
+	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	unsigned char k;
+	for(j=0; j<ms; j++)
+		for (k=0; k<4; k++) Timer3us(250);
+}
+
+void LCD_pulse (void)
+{
+	LCD_E=1;
+	Timer3us(40);
+	LCD_E=0;
+}
+
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x; //Send high nible
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_pulse();
+	Timer3us(40);
+	ACC=x; //Send low nible
+	LCD_D7=ACC_3;
+	LCD_D6=ACC_2;
+	LCD_D5=ACC_1;
+	LCD_D4=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_4BIT (void)
+{
+	LCD_E=0; // Resting state of LCD's enable is zero
+	// LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x32); // Change to 4-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x28);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	int j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
+
+int getsn (char * buff, int len)
+{
+	int j;
+	char c;
+	
+	for(j=0; j<(len-1); j++)
+	{
+		c=getchar();
+		if ( (c=='\n') || (c=='\r') )
+		{
+			buff[j]=0;
+			return j;
+		}
+		else
+		{
+			buff[j]=c;
+		}
+	}
+	buff[j]=0;
+	return len;
+}
+
+void InitADC (void)
+{
+	SFRPAGE = 0x00;
+	ADC0CN1 = 0b_10_000_000; //14-bit,  Right justified no shifting applied, perform and Accumulate 1 conversion.
+	ADC0CF0 = 0b_11111_0_00; // SYSCLK/32
+	ADC0CF1 = 0b_0_0_011110; // Same as default for now
+	ADC0CN0 = 0b_0_0_0_0_0_00_0; // Same as default for now
+	ADC0CF2 = 0b_0_01_11111 ; // GND pin, Vref=VDD
+	ADC0CN2 = 0b_0_000_0000;  // Same as default for now. ADC0 conversion initiated on write of 1 to ADBUSY.
+	ADEN=1; // Enable ADC
+}
+
+
+
+
+#define VDD 3.3035 // The measured value of VDD in volts
+
+void InitPinADC (unsigned char portno, unsigned char pinno)
+{
+	unsigned char mask;
+	
+	mask=1<<pinno;
+
+	SFRPAGE = 0x20;
+	switch (portno)
+	{
+		case 0:
+			P0MDIN &= (~mask); // Set pin as analog input
+			P0SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		case 1:
+			P1MDIN &= (~mask); // Set pin as analog input
+			P1SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		case 2:
+			P2MDIN &= (~mask); // Set pin as analog input
+			P2SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		default:
+		break;
+	}
+	SFRPAGE = 0x00;
+}
+
+unsigned int ADC_at_Pin(unsigned char pin)
+{
+	ADC0MX = pin;   // Select input from pin
+	ADBUSY=1;       // Dummy conversion first to select new pin
+	while (ADBUSY); // Wait for dummy conversion to finish
+	ADBUSY = 1;     // Convert voltage at the pin
+	while (ADBUSY); // Wait for conversion to complete
+	return (ADC0);
+}
+
+float Volts_at_Pin(unsigned char pin)
+{
+	 return ((ADC_at_Pin(pin)*VDD)/0b_0011_1111_1111_1111);
+}
+
+//function returns time while sinsusoid > 0 for one half-cycle
+/*float measure_half_cycle(float volts){
+ float total_time;
+ time_t  starting_time,ending_time;
+ starting_time = time(NULL);
+
+   if(volts == 0){
+     while(volts != 0){
+       ending_time = time(NULL);  
+          }
+     }
+   else{
+     total_time = 0.0;
+     return total_time;
+      }
+  total_time = ((ending_time-starting_time) / 1000000); //period/2
+  return total_time;     
+}*/ 
+float measure_half_cycle(float volts){
+// Measure half period at pin P1.0 using timer 0
+unsigned int half_Period; 
+TR0=0; // Stop timer 0
+TMOD=0B_0000_0001; // Set timer 0 as 16-bit timer
+TH0=0; TL0=0; // Reset the timer
+while (volts != 0); // Wait for the signal to be zero
+while (volts==0); // Wait for the signal to be one
+TR0=1; // Start timing
+while (volts != 0); // Wait for the signal to be zero
+TR0=0; // Stop timer 0
+// [TH0,TL0] is half the period in multiples of 12/CLK, so:
+half_Period=(TH0*0x100+TL0); // Assume Period is unsigned int
+return half_Period;
+}
+
+//measures the peak voltage once the half_cycle has been determined
+float measure_peak_voltage(float voltage, unsigned int half_cycle_time){
+  float peak_time = (half_cycle_time/2)*1000; //peak time in ms
+  float peak_voltage;
+
+    while(voltage != 0){
+    }
+      waitms(peak_time);
+      peak_voltage = voltage;
+       return peak_voltage; 
+}
+
+/*float get_phase_difference(float reference_voltage, float measured_voltage, unsigned int half_cycle_time){
+ float totalled_time,phase_diff;
+ time_t  start_time,end_time;
+ 
+ while(measured_voltage != 0){
+    }
+    start_time = time(NULL);
+    while(reference_voltage !=0){
+        }
+  
+  end_time = time(NULL);
+  totalled_time = ((end_time-start_time) / 1000000);
+  phase_diff = totalled_time*(360/(half_cycle_time*2));
+  return phase_diff;
+}*/
+
+float get_phase_difference(float reference_voltage, float measured_voltage, unsigned int half_cycle_time){
+  float phase_diff;
+  unsigned int totalled_time;
+
+// Measure half period at pin P1.0 using timer 0
+  TR0=0; // Stop timer 0
+  TMOD=0B_0000_0001; // Set timer 0 as 16-bit timer
+  TH0=0; TL0=0; // Reset the timer
+    while (measured_voltage!=0); // Wait for the signal to be zero
+      TR0=1; // Start timing
+    while (reference_voltage !=0); // Wait for the signal to be one
+      TR0=0; // Stop timer 0
+            // [TH0,TL0] is half the period in multiples of 12/CLK, so:
+      totalled_time=(TH0*0x100+TL0)*2; // Assume Period is unsigned int
+
+      phase_diff = totalled_time*(360/(half_cycle_time*2));
+      
+  return phase_diff;
+
+}
+
+
+void main (void)
+{
+	float v[4],peak_voltage,rms_voltage,phase_diff,signal_frequency;
+	char rms_value_string[16],phase_diff_string[16];
+	unsigned int half_cycle_time;
+	LCD_4BIT();
+	
+
+    waitms(500); // Give PuTTy a chance to start before sending
+	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
+	
+	printf ("ADC test program\n"
+	        "File: %s\n"
+	        "Compiled: %s, %s\n\n",
+	        __FILE__, __DATE__, __TIME__);
+	
+	InitPinADC(2, 2); // Configure P2.2 as analog input
+	InitPinADC(2, 3); // Configure P2.3 as analog input
+	InitPinADC(2, 4); // Configure P2.4 as analog input
+	InitPinADC(2, 5); // Configure P2.5 as analog input
+    InitADC();
+
+	while(1)
+	{
+	    // Read 14-bit value from the pins configured as analog inputs
+		v[0] = Volts_at_Pin(QFP32_MUX_P2_2);
+		v[1] = Volts_at_Pin(QFP32_MUX_P2_3);
+	    half_cycle_time = measure_half_cycle(v[0]);
+	    peak_voltage= measure_peak_voltage(v[0], half_cycle_time);
+	    phase_diff = get_phase_difference(v[1], v[0], half_cycle_time);
+	    rms_voltage = peak_voltage /1.41421356237 ;
+	    signal_frequency = 1/(half_cycle_time*2);
+	    
+	    //sprintf(string_name,"%f",variable);
+	    sprintf(rms_value_string,"%f",rms_voltage);
+	    sprintf(phase_diff_string,"%f",phase_diff);
+	    
+	    LCDprint(rms_value_string,1,1);
+	    LCDprint(phase_diff_string,2,1);
+	    
+		printf ("V@P2.2=%7.5fV, V@P2.3=%7.5fV, V@P2.4=%7.5fV, V@P2.5=%7.5fV\r", v[0], v[1], v[2], v[3]);
+		waitms(250);
+	 }  
+}	
+
